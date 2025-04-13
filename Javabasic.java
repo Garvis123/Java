5e2d88cdf3e343b0ab6061b9dd74b6e71800c079
Garvis123 <parvmodi11@gmail.com>
2025-04-13 23:58:56 +0530

    Add

--- Javabasic.java
+++ Javabasic.java
@@ -1,23 +1,18 @@
-import java.util.Scanner;
-
 public class Javabasic {
 
-  public boolean isPalindrome(int x) {
-    int reverse=0;
-    int b=x;
-    while(x>0)
-    {
-        int last = x%10;
-        reverse=last + (reverse*10);
-        x=x/10;
-      }
-      if(reverse=b)
-      {
-          return true;
-      }
-}
-    public static void main(String[] args) {
-      int x=121;
-      isPalindrome(x);
+  public static boolean isPalindr(int x) {
+    int reverse = 0;
+    int b = x;
+    while (x > 0) {
+      int last = x % 10;
+      reverse = last + (reverse * 10);
+      x = x / 10;
+    }
+    return reverse == b;
+  }
+
+  public static void main(String[] args) {
+    int x = 121;
+    System.out.println(isPalindr(x));  // ✅ Works now
   }
 }
