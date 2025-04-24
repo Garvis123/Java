da89d17a473169f85edfcef42ce7500d1274dbc3
Garvis123 <parvmodi11@gmail.com>
2025-04-24 22:00:15 +0530

    add

--- Array/PlusOne.java
+++ Array/PlusOne.java
@@ -1,15 +1,17 @@
+package Array;
+
 import java.util.Arrays;
 
-public class Javabasic{
-    public static int[] find(int digits[])
+public class PlusOne {
+        public static int[] find(int digits[])
     {
         int n=digits.length;
-        for(int i=n-1;i>0;i++)
+        for(int i=n-1;i>=0;i++)
         {
             if(digits[i]<9)
             {
                 digits[i]++;
-return digits;
+return digits;      
             }
             digits[i]=0;
         }
