67164fe7d9eab65d59f895a467dd4ece85b47f16
Garvis123 <parvmodi11@gmail.com>
2025-06-21 23:13:18 +0530

    ADD

--- Javabasic.java
+++ Javabasic.java
@@ -1,8 +1,12 @@
-public class Javabasic {
-
-    
-    public static void main(String args[])
-    {
-   
-    }
+public class Javabasic {  
+   public static void main(String args[])
+   {
+    int arr[]={1,2,3,4,5};
+     int largest=Integer.MIN_VALUE;
+     for(int i=0;i<arr.length;i++)
+     {
+        largest=Math.max(largest,arr[i]);
+     }
+     System.out.println(largest);
+   }
 }
