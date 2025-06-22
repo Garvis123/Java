47e61b2f2bcae93ebe9c1bacb6876e7bd8c87a4f
Garvis123 <parvmodi11@gmail.com>
2025-06-22 19:34:16 +0530

    ADD

---
+++ Sorting/QuickSort.java
@@ -0,0 +1,50 @@
+package Sorting;
+
+public class QuickSort {
+
+    public static void QuickSorting(int arr[],int si,int ei)
+    {
+        if(si>=ei)
+        {
+            return;
+        }
+        int partidx=partition(arr,si,ei);
+        QuickSorting(arr,si,partidx-1); //left
+        QuickSorting(arr,partidx+1,ei);//right
+    }
+
+    public static int partition(int arr[],int si,int ei)
+    {
+        int pivot=arr[ei];
+        int i=si-1;
+        
+        for(int j=si;j<ei;j++){
+            if(arr[j]<=pivot)
+            {
+                i++;
+                //swap
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+            }
+        }
+          i++;
+           int temp=pivot;
+                arr[ei]=arr[i];
+                arr[i]=temp;
+                return i;
+    }
+
+    public static void printArr(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String args[]){
+        int arr[]={4,2,1,6,7,5};
+        QuickSorting(arr,0,arr.length-1);
+        printArr(arr);
+    }
+}
