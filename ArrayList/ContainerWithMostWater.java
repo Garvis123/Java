d8bbf49d2ec0904f533a3df453acf8daee697c7a
Garvis123 <parvmodi11@gmail.com>
2025-09-07 16:20:11 +0530

    add

--- ArrayList/ContainerWithMostWater.java
+++ ArrayList/ContainerWithMostWater.java
@@ -4,20 +4,41 @@ import java.util.ArrayList;
 
 public class ContainerWithMostWater {
 
-    public static int storeWater(ArrayList<Integer> heigth)
+    // public static int storeWater(ArrayList<Integer> heigth)
+    // {
+    //     int maxWater=0;
+    //     for(int i=0;i<heigth.size();i++)
+    //     {
+    //         for(int j=i+1;j<heigth.size();j++)
+    //         {
+    //             int h=Math.min(heigth.get(i),heigth.get(j));
+    //             int w=j-i;
+    //             int currWater=h*w;
+    //             maxWater=Math.max(maxWater, currWater);
+    //         }
+    //     }
+    //     return maxWater;
+    // }
+
+    public static int storeWater(ArrayList<Integer> height)
     {
-        int maxWater=0;
-        for(int i=0;i<heigth.size();i++)
-        {
-            for(int j=i+1;j<heigth.size();j++)
-            {
-                int h=Math.min(heigth.get(i),heigth.get(j));
-                int w=j-i;
-                int currWater=h*w;
-                maxWater=Math.max(maxWater, currWater);
-            }
-        }
-        return maxWater;
+          int maxWater=0;
+          int i=0;
+          int j=height.size()-1;
+          while(i<j){
+             int ht=Math.min(height.get(i),height.get(j));
+             int wd=j-i;
+             int currWa=ht*wd;
+             maxWater=Math.max(maxWater, currWa);
+             if(height.get(i)<height.get(j))
+             {
+                i++;
+             }
+             else{
+                j--;
+             }
+          }
+          return maxWater;
     }
 
     public static void main(String[] args) {
