f537a3a1e038f747afbe9903f5b8ea59cda86186
Garvis123 <parvmodi11@gmail.com>
2025-11-12 23:23:52 +0530

    add

--- TopQuestions/RemoveDupli.java
+++ TopQuestions/RemoveDupli.java
@@ -1,7 +1,23 @@
 package TopQuestions;
 
 public class RemoveDupli {
+
+    public static int Duplicate(int nums[])
+    {
+       int k=0;
+       for(int i=1;i<nums.length;i++)
+       {
+        if(nums[i]!=nums[k])
+        {
+            k++;
+            nums[k]=nums[i];
+        }
+       }
+       return k+1;
+    }
     public static void main(String[] args) {
-        
+        int  nums[] ={1,1,2};
+        System.out.print(Duplicate(nums));
+
     }
 }
