b33ec63627cb7c23e452fcfff5512ec289fe56f4
Garvis123 <parvmodi11@gmail.com>
2025-11-14 21:43:51 +0530

    add

--- TopQuestions/MajorityElem.java
+++ TopQuestions/MajorityElem.java
@@ -1,8 +1,32 @@
 package TopQuestions;
 
 public class MajorityElem {
+
+    public static void majority(int nums[])
+    {
+        int candidate=nums[0];
+        int count=1;
+        for(int i=1;i<nums.length;i++)
+        {
+           if(nums[i]==candidate)
+           {
+            count++;
+           }
+           else
+           {
+            count--;
+            if(count==0)
+            {
+                candidate=nums[i];
+                count=1;
+            }
+           }
+        }
+        System.out.print(candidate);
+    }
     public static void main(String args[])
     {
         int nums[]={3,2,3};
+        majority(nums);
     }
 }
