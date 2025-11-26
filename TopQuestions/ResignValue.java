3e787ee162b2f5f3055d26773d47bc318f78f8c1
Garvis123 <parvmodi11@gmail.com>
2025-11-26 23:40:42 +0530

    add

---
+++ TopQuestions/ResignValue.java
@@ -0,0 +1,31 @@
+package TopQuestions;
+
+import java.util.*;
+
+public class ResignValue {
+    public static void resignValue(int nums[])
+    {
+        int positive =0;
+        int negative=0;
+        int arr[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]>0)
+            {
+                arr[positive]=nums[i];
+                positive+=2;
+            }
+            else
+            {
+                arr[negative]=nums[i];
+                negative+=2;
+            }
+        }
+    }
+    
+    public static void main(String args[])
+    {
+       int nums[] = {3,1,-2,-5,2,-4};
+       resignValue(nums);
+    }
+}
