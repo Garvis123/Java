1266826da008f55f7e95d00a3aed8c7d68660259
Garvis123 <parvmodi11@gmail.com>
2025-11-30 21:01:29 +0530

    Temp

---
+++ TopQuestions/DailyTemp.java
@@ -0,0 +1,34 @@
+package TopQuestions;
+
+
+public class DailyTemp {
+
+    public static  int[] change(int temp[])
+    {
+        int arr[]= new int[temp.length];
+        int k=0;
+        for(int i=0;i<temp.length;i++)
+        {
+          for(int j=i+1;j<temp.length;j++)
+          {
+            if(temp[i]<temp[j])
+            {
+                arr[k]=j-i;
+                k++;
+                break;
+            }
+          }
+        }
+        return arr;
+    }
+    public static void main(String args[])
+    {
+        int temp[]={73,74,75,71,69,72,76,73};
+       int ans[]= change(temp);
+       for(int i=0;i<ans.length;i++)
+       {
+        System.out.print(ans[i] + " ");
+       }
+
+    }
+}
